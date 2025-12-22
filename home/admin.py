7c6363b31fc7d63b87d9b4b5ac64163b1daf9c4c
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-22 18:34:31 +0545

    5th

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Student
+
+
+admin.site.register(Student)
+
 
 # Register your models here.
+
+# student
+# student
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
@@ -7,6 +8,15 @@ def student_List(request):
     return render(request,'student_list.html')
 
 def student_Add(request):
+    if request.method == 'POST':
+        name= request.POST.get('name')
+        age = request.POST.get('age')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+
+        student = Student(name= name,age=age,email=email,address=address)
+        student.save()
+
     return render(request,'student_add.html')
 
 def student_delete(request):
