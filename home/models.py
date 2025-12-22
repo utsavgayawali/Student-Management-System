1f0da43f81fa1fc4463f298e77ab52078177cc83
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-22 18:27:58 +0545

    4th

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=50)
+    age = models.IntegerField()
+    address = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+    
+
