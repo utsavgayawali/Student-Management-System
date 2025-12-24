da6120166db8f7ce4d94e151f4d0c65ac11f7a0c
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-24 11:18:37 +0545

    7th commit

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,8 @@ from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student_list/', views.student_List, name='student_list'),
-    path('add/', views.student_Add, name='student_add'),
-    path('delete/', views.student_delete, name='student_delete')
+    path('student_List/', views.student_List, name='student_List'),
+    path('add/', views.student_Add, name='student_Add'),
+    path('edit/<int:student_id>/', views.student_Edit, name='student_Edit'),
+    path('delete/<int:student_id>/', views.student_Delete, name='student_Delete')
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Student
 
 # Create your views here.
 
 
 def student_List(request):
-    return render(request,'student_list.html')
+    student_info = Student.objects.all()
+    return render(request,'student_list.html',{'student_info':student_info})
+
+
 
 def student_Add(request):
     if request.method == 'POST':
@@ -16,9 +19,42 @@ def student_Add(request):
 
         student = Student(name= name,age=age,email=email,address=address)
         student.save()
-        return redirect('student_list')
+        return redirect('student_List')
     else:
         return render(request,'student_add.html')
 
-def student_delete(request):
-    return render(request,'student_delete.html')
+
+
+
+
+def student_Edit(request,student_id):
+        student =get_object_or_404(Student,pk=student_id)
+        if request.method == 'POST':
+            student.name= request.POST.get('name')
+            student.email = request.POST.get('email')
+            student.age = request.POST.get('age')
+            student.address = request.POST.get('address')
+            student.save()
+            return redirect('student_List')
+        else:
+            return render(request,'student_add.html',{'student':student})
+
+
+
+
+
+def student_Delete(request,student_id):
+    if request.method == 'POST':
+        student = get_object_or_404(Student,pk=student_id)
+        student.delete()
+        return redirect('student_List')
+    else:
+        return render(request,'student_delete.html')
+
+
+
+
+
+
+
+
